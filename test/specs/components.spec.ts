e2660b3664bcd96578dca9268336be9549370f82
scottbedard <scottbedard90@gmail.com>
2018-08-31 20:30:09 -0700

    skip templates

--- test/specs/components.spec.ts
+++ test/specs/components.spec.ts
@@ -8,29 +8,31 @@ import Compiler from 'src/compiler/compiler';
 describe('components', () => {
     const parserDir = path.resolve(__dirname, 'components');
 
-    fs.readdirSync(parserDir).forEach(file => {
-        const source = fs.readFileSync(path.resolve(__dirname, './components', file, './component.vue'), 'utf8');
-        const test = require(path.resolve(__dirname, './components', file, './test.ts'));
+    fs.readdirSync(parserDir)
+        .filter(file => file !== '_template')
+        .forEach(file => {
+            const source = fs.readFileSync(path.resolve(__dirname, './components', file, './component.vue'), 'utf8');
+            const test = require(path.resolve(__dirname, './components', file, './test.ts'));
 
-        const testFn = function () {
-            // compile our component to a function
-            const compiler = new Compiler({
-                cleanOutput: true,
-                format: 'fn',
-                name: 'Component',
-                ...(test.options || {}),
-            }, source);
+            const testFn = function () {
+                // compile our component to a function
+                const compiler = new Compiler({
+                    cleanOutput: true,
+                    format: 'fn',
+                    name: 'Component',
+                    ...(test.options || {}),
+                }, source);
 
-            const output = compiler.compile();
+                const output = compiler.compile();
 
-            if (typeof test.test === 'function') {
-                test.test(output, new Function(output.code)());
+                if (typeof test.test === 'function') {
+                    test.test(output, new Function(output.code)());
+                }
             }
-        }
 
-        // focus, skip, or standard run the test
-        if (test.only) it.only(file, testFn);
-        else if (test.skip) it.skip(file, testFn);
-        else it(file, testFn);
-    });
+            // focus, skip, or standard run the test
+            if (test.only) it.only(file, testFn);
+            else if (test.skip) it.skip(file, testFn);
+            else it(file, testFn);
+        });
 });
--- test/specs/parser.spec.ts
+++ test/specs/parser.spec.ts
@@ -10,45 +10,47 @@ import { expect } from 'chai';
 describe('parser', () => {
     const parserDir = path.resolve(__dirname, 'parser');
 
-    fs.readdirSync(parserDir).forEach(file => {
-        const source = fs.readFileSync(path.resolve(__dirname, './parser', file, './component.vue'), 'utf8');
-        const test = require(path.resolve(__dirname, './parser', file, './test.ts'));
+    fs.readdirSync(parserDir)
+        .filter(file => file !== '_template')
+        .forEach(file => {
+            const source = fs.readFileSync(path.resolve(__dirname, './parser', file, './component.vue'), 'utf8');
+            const test = require(path.resolve(__dirname, './parser', file, './test.ts'));
 
-        const testFn = function () {
-            // create a compiler for the component
-            const compiler = new Compiler({
-                name: 'Component',
-                ...(test.options || {}),
-            }, source);
+            const testFn = function () {
+                // create a compiler for the component
+                const compiler = new Compiler({
+                    name: 'Component',
+                    ...(test.options || {}),
+                }, source);
 
-            // parse the component
-            compiler.parse();
+                // parse the component
+                compiler.parse();
 
-            const excludeType = typeof test.exclude;
-            const includeType = typeof test.include;
-            const testType = typeof test.test;
-            
-            // make sure all included/excluded properties are present/absent
-            if (includeType !== 'undefined') {
-                expect(includeType).to.equal('object', 'Invalid parser include');
-                expect(compiler.parsedSource).to.containSubset(test.include);
+                const excludeType = typeof test.exclude;
+                const includeType = typeof test.include;
+                const testType = typeof test.test;
+                
+                // make sure all included/excluded properties are present/absent
+                if (includeType !== 'undefined') {
+                    expect(includeType).to.equal('object', 'Invalid parser include');
+                    expect(compiler.parsedSource).to.containSubset(test.include);
+                }
+
+                if (excludeType !== 'undefined') {
+                    expect(excludeType).to.equal('object', 'Invalid parser exclude');
+                    expect(compiler.parsedSource).not.to.containSubset(test.exclude);
+                }
+
+                // run the test function
+                if (testType === 'function') {
+                    expect(testType).to.equal('function', 'Invalid parser test');
+                    test.test(compiler);
+                }
             }
 
-            if (excludeType !== 'undefined') {
-                expect(excludeType).to.equal('object', 'Invalid parser exclude');
-                expect(compiler.parsedSource).not.to.containSubset(test.exclude);
-            }
-
-            // run the test function
-            if (testType === 'function') {
-                expect(testType).to.equal('function', 'Invalid parser test');
-                test.test(compiler);
-            }
-        }
-
-        // focus, skip, or standard run the test
-        if (test.only) it.only(file, testFn);
-        else if (test.skip) it.skip(file, testFn);
-        else it(file, testFn);
-    });
+            // focus, skip, or standard run the test
+            if (test.only) it.only(file, testFn);
+            else if (test.skip) it.skip(file, testFn);
+            else it(file, testFn);
+        });
 });
